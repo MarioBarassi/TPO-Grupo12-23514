ba997227038f964db663c10b6d16a18a831924ac
German Quercia <gerquercia@hotmail.com>
2023-10-27 03:09:31 -0300

    Update hamb.js

--- script/hamb.js
+++ script/hamb.js
@@ -1,8 +1,6 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
-const abrirID = document.querySelector("#abrir");
-const cerrarID = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
